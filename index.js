2b98d8a060f4b67b5033eae11a1ffec502890f39
Dan Hintz <dhintz@live.com>
2019-06-23 01:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function logDriverNames(arr) {
+  arr.forEach(function(el) {
+    console.log(el.name);
+  });
+}
+
+function logDriversByHometown(arr, location) {
+  arr.forEach(function(el) {
+    if (el.hometown === location) {
+      console.log(el.name);
+    }
+  });
+}
+
+function driversByRevenue(arr) {
+  const sortedList = arr.sort(function compare(a,b){
+      if(a.revenue > b.revenue) {
+        return 1;
+      } else if (a.revenue < b.revenue) {
+        return -1;
+      } else {
+        return 0;
+      }
+  });
+  return sortedList;
+}
+
+function driversByName(arr) {
+  const sortedList = arr.sort(function compare(a,b) {
+    a.name.localeCompare(b.name);
+  });
+  return sortedList;
+}
